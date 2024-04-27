2cfe82adb40c4e1715774a7a00f4eef6ecdb1c97
Mark Ryan <markrryan99@gmail.com>
2024-04-27 14:46:48 -0700

    stuff

--- catcollector_project/catcollector_app/views.py
+++ catcollector_project/catcollector_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# views.py
+
+# Add this cats list below the imports
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 def home(request):
     # return HttpResponse('hello')
@@ -8,3 +16,9 @@ def home(request):
 
 def about(request):
     return render(request, 'cats/about.html')
+
+def cats_index(request):
+    # passing data extremely close to ejs
+    return render(request, 'cats/index.html', {
+        'cats' : cats
+    })
